346396c6575b685739eaee65d8a4aa167e8b78d5
Daniel Mast <danielmastnor@gmail.com>
2022-07-15 14:00:51 -0400

    Update truck acceleration

--- Assets/Player/TruckController.cs
+++ Assets/Player/TruckController.cs
@@ -6,7 +6,19 @@ using UnityEngine;
 public class TruckController : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] float acceleration;
+    [SerializeField] float torque = 5000f;
+    [SerializeField] float breakForce = 10000f;
+    [SerializeField] float reverseForce = 1000f;
+    [SerializeField] float wheelTurnAccelerator = 1f;
+    [SerializeField] float modifier = 100000f;
+
+    // Initialize speed variables
+    float verticalAcceleration = 0f;
+    float turnAcceleration = 0f;
+    Vector2 acceleration = Vector2.zero;
+    float xVelocity;
+    float force;
+    
 
     Vector2 userInput;
 
@@ -26,9 +38,42 @@ public class TruckController : MonoBehaviour
         userInput = i;
     }
 
-    private void Move(Vector2 i)
+    private void Move(Vector2 input)
     {
-        rb.AddForce(i * acceleration * Time.deltaTime);
+        // Check for user input
+        if (input == Vector2.zero) return;
+
+
+        // Find what type of acceleration to do based on input
+        if (input.y != 0){       
+
+            // is truck direction the same as user direction?
+            xVelocity = transform.InverseTransformDirection(rb.velocity).y; // Code taken from https://answers.unity.com/questions/32551/how-do-i-obtain-the-local-velocity-of-a-rigid-body.html
+            
+
+            // breaking using break force
+            if (xVelocity > 0 && input.y < 0 || xVelocity < 0 && input.y > 0) {
+                Debug.Log("Breaking");
+                force = breakForce;}
+
+            //accelerating using torque
+            else if (input.y > 0){
+                Debug.Log("Accelerating");
+                force = torque;}
+                
+            // reversing using reverse force
+            else {
+                Debug.Log("Reversing");
+                force = reverseForce;}
+            
+            // Apply acceleration
+            verticalAcceleration = (input.y * force);
+            acceleration.y = verticalAcceleration;
+        }
+
+        // Apply torque to vehicle
+        rb.AddRelativeForce(acceleration * modifier * Time.deltaTime);
+        
     }
 
     private void OnEnable()
